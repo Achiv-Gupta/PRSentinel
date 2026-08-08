7ab6bb406cd8bbec03e5d585e1b21c2a2d35b2d2
Achiv Gupta <achivgupta@Achivs-MacBook-Air.local>
2026-08-09 00:43:09 +0530

    Handle Github webhook events & persist pull requests

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,45 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import PullRequest
+
 
 @api_view(["POST"])
 def github_webhook(request):
-    print(request.data)
 
-    return Response({"message": "Webhook received"})
+    payload = request.data
+
+    #.get because it can be none and still there will be no error
+    action = payload.get("action")
+
+    if action not in ["opened", "reopened", "synchronize", "closed"]:
+        return Response({"message": "Ignored"})
+
+    pr = payload["pull_request"]
+
+    repository = payload["repository"]["full_name"]
+    pr_number = pr["number"]
+    title = pr["title"]
+    author = pr["user"]["login"]
+    commit_sha = pr["head"]["sha"]
+    status = pr["state"]
+
+    # print(repository)
+    # print(pr_number)
+    # print(title)
+    # print(author)
+    # print(commit_sha)
+    # print(status)
+
+    PullRequest.objects.update_or_create(
+    repository=repository,
+    pr_number=pr_number,
+    defaults={
+        "title": title,
+        "author": author,
+        "commit_sha": commit_sha,
+        "status": status.upper(),
+    },
+)
+
+    return Response({"message": "Saved"})
