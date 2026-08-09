1626c97507f9febf1282f1ac160b103c6cb72370
Achiv Gupta <achivgupta@Achivs-MacBook-Air.local>
2026-08-09 15:02:51 +0530

    Refactor of files

---
+++ reviews/services.py
@@ -0,0 +1,34 @@
+from .models import PullRequest
+from .github.client import GitHubClient
+
+
+def process_pull_request(payload):
+    action = payload.get("action")
+
+    if action not in ["opened", "reopened", "synchronize", "closed"]:
+        return
+
+    pr = payload["pull_request"]
+
+    repository = payload["repository"]["full_name"]
+    pr_number = pr["number"]
+    title = pr["title"]
+    author = pr["user"]["login"]
+    commit_sha = pr["head"]["sha"]
+    status = pr["state"]
+
+    PullRequest.objects.update_or_create(
+        repository=repository,
+        pr_number=pr_number,
+        defaults={
+            "title": title,
+            "author": author,
+            "commit_sha": commit_sha,
+            "status": status.upper(),
+        },
+    )
+
+    client = GitHubClient()
+    files = client.get_pull_request_files(repository, pr_number)
+
+    return files
--- reviews/views.py
+++ reviews/views.py
@@ -1,65 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .services import process_pull_request
 
-from .models import PullRequest
-from .github.client import GitHubClient
 
 @api_view(["POST"])
 def github_webhook(request):
+    files = process_pull_request(request.data)
 
-    payload = request.data
+    if files:
+        for file in files:
+            print(file["filename"])
+            print(file["patch"])
 
-    #.get because it can be none and still there will be no error
-    action = payload.get("action")
-
-    if action not in ["opened", "reopened", "synchronize", "closed"]:
-        return Response({"message": "Ignored"})
-
-    pr = payload["pull_request"]
-
-    repository = payload["repository"]["full_name"]
-    pr_number = pr["number"]
-    title = pr["title"]
-    author = pr["user"]["login"]
-    commit_sha = pr["head"]["sha"]
-    status = pr["state"]
-
-    # print(repository)
-    # print(pr_number)
-    # print(title)
-    # print(author)
-    # print(commit_sha)
-    # print(status)
-
-    PullRequest.objects.update_or_create(
-    repository=repository,
-    pr_number=pr_number,
-    defaults={
-        "title": title,
-        "author": author,
-        "commit_sha": commit_sha,
-        "status": status.upper(),
-    },
-    )
-    client = GitHubClient()
-
-    files = client.get_pull_request(
-        repository,
-        pr_number
-    )
-
-    for file in files:
-
-        print("=" * 50)
-
-        print(file["filename"])
-
-        print(file["status"])
-
-        print(file["patch"])
-
-        print("=" * 50)
-
-    return Response({"message": "Saved"})
-
-
+    return Response({"message": "Webhook received"})
