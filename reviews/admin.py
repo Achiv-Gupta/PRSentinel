00d7e5e2e7d299114697b2f88ac8fdce75eaeb35
Achiv Gupta <achivgupta@Achivs-MacBook-Air.local>
2026-08-12 21:47:00 +0530

    Improve Django admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
+
 from .models import PullRequest, Review
 
-admin.site.register(PullRequest)
-admin.site.register(Review)
+
+@admin.register(PullRequest)
+class PullRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "repository",
+        "pr_number",
+        "title",
+        "author",
+        "status",
+        "created_at",
+    )
+
+    list_filter = ("status",)
+
+    search_fields = (
+        "repository",
+        "title",
+        "author",
+    )
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "repository",
+        "pr_number",
+        "commit_sha",
+        "status",
+        "created_at",
+    )
+
+    list_filter = ("status",)
+
+    search_fields = (
+        "repository",
+        "commit_sha",
+    )
